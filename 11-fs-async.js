aff348fc770a298705caabe044ebf5c7bf830d39
AngelaBewick <angelagracebewick@gmail.com>
2022-06-06 15:25:56 -0500

    fs-async and http

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,9 +1,40 @@
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
 
-readFile("./content/first.txt", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile); //or add .promises to the end of the fs import
+
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME : ${first} ${second}`
+    );
+
+    console.log(first, "complete");
+    console.log(second, "complete");
+  } catch (error) {
+    console.log(error);
   }
-  console.log(result);
-});
+};
+start();
+console.log("second");
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
